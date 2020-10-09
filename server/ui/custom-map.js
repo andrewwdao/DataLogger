bc408c13c165c330a033b682f0ac2f25a7aebb07
Anh Khoi Tran <khoitrananh.20@gmail.com>
2020-10-09 12:02:39 +0700

    Update map search bar, fetch data dynamically

--- server/ui/custom-map.js
+++ server/ui/custom-map.js
@@ -2,6 +2,24 @@
 
 const HOST = 'http://datalogger.ddns.net:8080';
 
+let stations = [
+    // {
+    //     id: 'CT33',
+    //     name: 'D114. Lê Thị Hồng Gấm',
+    //     lat: 10.05,
+    //     lng: 105.74,
+    //     lastUpdate: '13/09/2020 20:19',
+    //     data: {
+    //         'AL (2)': 0.76,
+    //     }
+    // }
+];
+
+function windowResize() {
+    let options = document.getElementById("nav-node-search-options");
+    options.style.maxHeight = Math.min(window.innerHeight - options.offsetTop - 100, 300) + "px";
+}
+
 function togglePageOverlay() {
     document.getElementById("page-overlay").classList.toggle("show");
 }
@@ -17,18 +35,30 @@ function toggleDetailNodeLabel(sw) {
     document.getElementById('detail-node-label-flag').classList.toggle('active');
 }
 
-let stations = [
-    // {
-    //     id: 'CT33',
-    //     name: 'D114. Lê Thị Hồng Gấm',
-    //     lat: 10.05,
-    //     long: 105.74,
-    //     lastUpdate: '13/09/2020 20:19',
-    //     data: {
-    //         'AL (2)': 0.76,
-    //     }
-    // }
-];
+function updateNavNodeSearchOptions(filter) {
+    let options = document.getElementById("nav-node-search-options");
+    options.innerHTML = '';
+    stations.forEach(station => {
+        let optionValue = `${station.id} - ${station.name}`;
+        let i = 0;
+        if (!filter || optionValue.indexOf(filter) != -1) {
+            let option = document.createElement('div');
+            option.className = 'option';
+            option.innerHTML = optionValue;
+            option.onclick = () => {
+                station.marker.openPopup();
+            };
+            options.appendChild(option);
+        }
+    });
+
+    if (!options.innerHTML) {
+        options.innerHTML = `
+            <div class="option">(Không có dữ liệu)</div>
+        `;
+    }
+}
+
 
 let mymap = L.map('main-map').setView([9.997626, 105.728626], 15);
 
@@ -62,34 +92,45 @@ function fetchStationData(station) {
 }
 
 function updateStationPopup(station) {
-    let stationData = '';
-    for (let key in station.data) {
+    let stationData = `
+        <div class="row">
+            <span class="label">Thời điểm</span>
+            <span class="value">
+                ${station.data.timestamp ?
+                    new Date(station.data.timestamp*1000).toLocaleDateString() + ' ' + new Date(station.data.timestamp*1000).toLocaleTimeString() :
+                    'Chưa có thông tin'}
+            </span>
+        </div>
+    `;
+    for (let key in station.data.payload) {
         stationData += `
             <div class="row">
                 <span class="label">${key}</span>
-                <span class="value">${station.data[key]}</span>
+                <span class="value">${station.data.payload[key]}</span>
             </div>
         `;
     }
 
     station.marker._popup.setContent(`
         <div class="header">${station.id + ' - ' + station.name}</div>
-            <div class="row">
+            <!--<div class="row">
                 <span class="label">Thời điểm</span>
                 <span class="value">${station.lastUpdate ? station.lastUpdate : 'Chưa có thông tin'}</span>
-            </div>
+            </div>-->
             ${stationData}
             <div onclick="toggleNodeDetailModal()" class="footer">Thêm thông tin</div>
         <div class="footer-float-fix">a</div>
     `);
 }
 
-function addStation(id, name, lat, long) {
+function addStation(id, name, lat, lng) {
     let active = Math.random() >= 0.3;
 
     let station = {
         id: id,
         name: name,
+        lat: lat,
+        lng: lng,
         data: {
             'Thông số': 'Chưa có thông tin'
         }
@@ -112,7 +153,7 @@ function addStation(id, name, lat, long) {
             </div>
         `,
     });
-    station.marker = L.marker([lat, long], {
+    station.marker = L.marker([lat, lng], {
         icon: markerIcon
     }).addTo(mymap);
 
@@ -152,6 +193,7 @@ function fetchStations() {
                 fStations.forEach(station => {
                     addStation(station['station_code'], station['station_address'], station['station_latitude'], station['station_longitude']);
                 });
+                updateNavNodeSearchOptions();
             }
         }
     );
@@ -159,6 +201,11 @@ function fetchStations() {
 
 // addStation('CT33', 'D114. Lê Thị Hồng Gấm', 10.05, 105.74);
 fetchStations();
+windowResize();
+window.addEventListener("resize", windowResize);
+document.getElementById("nav-node-search-input").addEventListener("keyup", () => {
+    updateNavNodeSearchOptions(document.getElementById("nav-node-search-input").value);
+});
 
 // var popup = L.popup()
 //     .setLatLng([10.05, 105.74])
